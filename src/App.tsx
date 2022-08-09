01b723c326e8bb8dfb7e239f490e1eb5d8017f84
Vladislav <simwebkz@gmail.com>
2022-08-10 01:27:07 +0600

    add stick menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,36 @@ import RouteList from "./RouterList";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
 import { Suspense } from "react";
+import StickMenu from "./components/StickMenu";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: "#14b890",
-            contrastText: "#ffffff",
-        },
+  palette: {
+    primary: {
+      main: "#14b890",
+      contrastText: "#ffffff",
     },
+  },
 });
 
 function App() {
-    return (
-        <ThemeProvider theme={theme}>
-            <div className="wrapper">
-                <Router>
-                    <Header />
-                    <main className="content">
-                        <Container>
-                            <Suspense>
-                                <RouteList />
-                            </Suspense>
-                        </Container>
-                    </main>
-                    <Footer />
-                </Router>
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="wrapper">
+        <Router>
+          <Header />
+          <main className="content">
+            <Container>
+              <Suspense>
+                <RouteList />
+              </Suspense>
+            </Container>
+          </main>
+          <StickMenu />
+          <Footer />
+        </Router>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/StickMenu/index.tsx
+++ src/components/StickMenu/index.tsx
@@ -1,11 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { Box, Button } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 
 const StickMenu = () => {
   const cn = classNames.bind(styles);
-  return <div></div>;
+  return (
+    <nav className={cn("nav")}>
+      <ul className={cn("list")}>
+        <li className={cn("list-item")}>
+          <Link className={cn("link")} to="/">
+            <Button variant="contained">Список игр</Button>
+          </Link>
+        </li>
+        <li className={cn("list-item")}>
+          <Link
+            className={cn("link")}
+            style={{ display: "inline-block", padding: "0.25rem" }}
+            to="/"
+          >
+            <Button variant="contained">
+              <AddIcon sx={{ fill: "white" }} fontSize="medium" />
+            </Button>
+          </Link>
+        </li>
+        <li className={cn("list-item")}>
+          <Link className={cn("link")} to="/">
+            <Button variant="contained">Вход</Button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default StickMenu;
